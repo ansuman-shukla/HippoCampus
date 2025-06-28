7d025e10490f999c14214ba567da98e54a7ac91d
Parasite <ansuman0026@gmail.com>
2025-06-28 17:48:56 +0530

    Update InputForm component to conditionally render button based on showOnlyOne state
    - Modified the button rendering logic to only display the button when showOnlyOne is false, enhancing user experience by reducing unnecessary UI elements.

--- frontend/src/components/InputForm.tsx
+++ frontend/src/components/InputForm.tsx
@@ -130,14 +130,16 @@ export default function InputForm({
       </div>)}
 
         <div className="flex justify-center mt-0">
-          <button
-            type="button"
-            className="text-neutral-700 bg-white/20 rounded-full px-4 py-2 flex justify-center items-center gap-1 mt-2  text-sm transition hover:text-black"
-            onClick={() => setShowNotes(!showNotes)}
-            disabled={isLoading}
-          >
-            <BsChevronDoubleDown size={12} /> { showNotes ? "Add Bookmarks" : "Add Notes"}
-          </button>
+          {!showOnlyOne && (
+            <button
+              type="button"
+              className="text-neutral-700 bg-white/20 rounded-full px-4 py-2 flex justify-center items-center gap-1 mt-2  text-sm transition hover:text-black"
+              onClick={() => setShowNotes(!showNotes)}
+              disabled={isLoading}
+            >
+              <BsChevronDoubleDown size={12} /> { showNotes ? "Add Bookmarks" : "Add Notes"}
+            </button>
+          )}
         </div>
 
       {Error ? (
